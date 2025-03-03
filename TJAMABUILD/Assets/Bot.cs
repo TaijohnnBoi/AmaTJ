42fbc46918d990115ad52a1ce7cb816b845054a1
Taijohn Jones <145067431+TaijohnnBoi@users.noreply.github.com>
2025-03-02 22:41:18 -0700

    Almost done
    yay

--- TJAMABUILD/Assets/Bot.cs
+++ TJAMABUILD/Assets/Bot.cs
@@ -7,10 +7,12 @@ public class Bot : MonoBehaviour
 {
     NavMeshAgent agent;
     public GameObject target;
+    Drive ds;
 
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
+        ds = target.GetComponent<Drive>();
     }
 
     void Seek(Vector3 location)
@@ -23,9 +25,55 @@ public class Bot : MonoBehaviour
         Vector3 fleevector = location - this.transform.position;
         agent.SetDestination(this.transform.position - fleevector);
     }
+
+    void Pursue()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+
+        float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+
+        float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
+
+        if ((toTarget > 90 && relativeHeading < 20) || ds.currentSpeed < 0.01f)
+        {
+            Seek(target.transform.position);
+            return;
+        }
+
+        float lookAhead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
+        Seek(target.transform.position + target.transform.forward * lookAhead);
+
+    }
+
+    void Evade()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+        float lookAhead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
+        Flee(target.transform.position + target.transform.forward * lookAhead);
+    }
+
+    Vector3 wanderTarget = Vector3.zero;
+    void Wander()
+    {
+        float wanderRadius = 10;
+        float wanderDistance = 10;
+        float wanderJitter = 1;
+
+        wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter, 
+                                        0, 
+                                        Random.Range(-1.0f, 1.0f) * wanderJitter);
+        wanderTarget.Normalize();
+        wanderTarget *= wanderRadius;
+
+        Vector3 targetLocal = wanderTarget + new Vector3(0, 0, wanderDistance);
+        Vector3 targetWorld = this.gameObject.transform.InverseTransformVector(targetLocal);
+
+        Seek(targetWorld);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Seek(target.transform.position);
+        Wander();
     }
 }
